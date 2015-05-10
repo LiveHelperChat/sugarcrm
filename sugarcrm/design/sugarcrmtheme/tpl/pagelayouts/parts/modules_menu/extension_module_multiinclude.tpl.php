2fb65e4fda25f7d1be41776224e9a759687bf193
Remdex <remdex@gmail.com>
2015-05-10 16:28:02 +0200

    Title in different template

--- sugarcrm/design/sugarcrmtheme/tpl/pagelayouts/parts/modules_menu/extension_module_multiinclude.tpl.php
+++ sugarcrm/design/sugarcrmtheme/tpl/pagelayouts/parts/modules_menu/extension_module_multiinclude.tpl.php
@@ -1,4 +1,4 @@
 <?php include(erLhcoreClassDesign::designtpl('lhsugarcrm/sugarcrm_tab_enabled_pre.tpl.php')); ?>
 <?php if (erLhcoreClassUser::instance()->hasAccessTo('lhsugarcrm','configure') && $sugarcrm_tab_enabled_pre == true) : ?>
-<li><a href="<?php echo erLhcoreClassDesign::baseurl('sugarcrm/index')?>">SugarCRM</a></li>
+<li><a href="<?php echo erLhcoreClassDesign::baseurl('sugarcrm/index')?>"><?php include(erLhcoreClassDesign::designtpl('lhsugarcrm/sugarcrm_title.tpl.php')); ?></a></li>
 <?php endif;?>
