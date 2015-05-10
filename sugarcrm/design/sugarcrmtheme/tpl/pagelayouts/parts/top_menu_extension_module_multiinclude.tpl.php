2572af65a71db26aa38658b5ebbd74b9418c17f6
Remdex <remdex@gmail.com>
2015-05-10 14:57:03 +0200

    Hide from top menu sugacrm if not enabled

--- sugarcrm/design/sugarcrmtheme/tpl/pagelayouts/parts/top_menu_extension_module_multiinclude.tpl.php
+++ sugarcrm/design/sugarcrmtheme/tpl/pagelayouts/parts/top_menu_extension_module_multiinclude.tpl.php
@@ -1,1 +1,4 @@
+<?php include(erLhcoreClassDesign::designtpl('lhsugarcrm/sugarcrm_tab_enabled_pre.tpl.php')); ?>
+<?php if ($sugarcrm_tab_enabled_pre == true) : ?>
 <?php $hasExtensionModule = $currentUser->hasAccessTo('lhsugarcrm','configure') || (isset($hasExtensionModule) ? $hasExtensionModule : false);?>
+<?php endif;?>
