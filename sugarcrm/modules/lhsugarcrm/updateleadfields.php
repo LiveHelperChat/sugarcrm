e9c0b84eba08b3da93818bc4e3383567f411a811
Donatas <doncaz111@gmail.com>
2016-08-31 09:44:50 +0300

    dispacher added

--- sugarcrm/modules/lhsugarcrm/updateleadfields.php
+++ sugarcrm/modules/lhsugarcrm/updateleadfields.php
@@ -1,15 +1,34 @@
-<?php 
+<?php
 
-$sugarcrm = erLhcoreClassModule::getExtensionInstance('erLhcoreClassExtensionSugarcrm');
+$chat = erLhcoreClassChat::getSession()->load( 'erLhcoreClassModelChat', $Params['user_parameters']['chat_id']);
 
-$lead = $sugarcrm->doUpdateLeadId($Params['user_parameters']['lead_id']);
+if ( erLhcoreClassChat::hasAccessToRead($chat) )
+{
+    $errors = [];
 
-$tpl = erLhcoreClassTemplate::getInstance('lhsugarcrm/getleadfields.tpl.php');
+    erLhcoreClassChatEventDispatcher::getInstance()->dispatch('sugarcrm.createorupdatelead', array('errors' => & $errors));
 
-$tpl->set('lead',$lead);
-$tpl->set('lead_updated',true);
+    if (empty($errors)) {
+        try {
+            $sugarcrm = erLhcoreClassModule::getExtensionInstance('erLhcoreClassExtensionSugarcrm');
+            $leadId = $sugarcrm->createLeadByChat($chat);
 
-echo json_encode(array('result' => $tpl->fetch()));
+            $tpl = erLhcoreClassTemplate::getInstance('lhsugarcrm/createorupdatelead.tpl.php');
+            $tpl->set('lead',$leadId);
+            $tpl->set('chat',$chat);
+
+            echo json_encode(array('result' => $tpl->fetch()));
+        } catch (Exception $e) {
+            $tpl = erLhcoreClassTemplate::getInstance('lhkernel/validation_error.tpl.php');
+            $tpl->set('errors',array($e->getMessage()));
+            echo json_encode(array('result' => $tpl->fetch()));
+        }
+    } else {
+        $tpl = erLhcoreClassTemplate::getInstance('lhkernel/validation_error.tpl.php');
+        $tpl->set('errors',$errors);
+        echo json_encode(array('result' => $tpl->fetch()));
+    }
+}
 
 exit;
 ?>
